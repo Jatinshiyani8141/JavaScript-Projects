6cafe4c67bd26d5d15836063f2e10111d19c02c8
Jatinshiyani8141 <jatinshiyani81414@gmail.com>
2026-02-14 19:21:18 -0500

    Added Basic JavaScript Projects with Project1_expressions_alert

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,15 @@
+//window.alert 
+window.alert("Hello, world");
+
+//documwnt.write
+document.write("Hello, COD ");
+
+//assign variables
+var sent1 = "This is first code "
+var sent2 = "using JavScript."
+
+//Display result
+document.write(sent1 + sent2);
+
+// an expression
+document.write(23 * 10);
